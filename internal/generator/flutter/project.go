70846f7d9834f950ac35cd6d47ea799b1c329d28
Lucas Bernardo <lbernardo.brito@gmail.com>
2024-06-02 07:50:21 -0300

    :hammer: fix create flutter project -> install dependencies

--- internal/generator/flutter/project.go
+++ internal/generator/flutter/project.go
@@ -15,7 +15,7 @@ func NewProject(params map[string]string, rootPath string) {
 		fmt.Println("error create flutter project:", err)
 		os.Exit(1)
 	}
-	cm = execute.Execute("flutter", "pub", "get", "flutter_modular", "mobx", "flutter_mobx", "mobx_codegen", "icons_plus", "google_fonts")
+	cm = execute.Execute("flutter", "pub", "add", "flutter_modular", "mobx", "flutter_mobx", "mobx_codegen", "icons_plus", "google_fonts")
 	cm.Dir = path.Join(rootPath, name)
 	if err := cm.Run(); err != nil {
 		fmt.Println("error get depedencies:", err)
